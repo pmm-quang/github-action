c4758c44224a979a0c8eaf06a2dcf1d4e5b1c856
qpmm <pmmquang@gmail.com>
2026-03-05 18:36:19 +0900

    fix format code

--- src/main/java/com/example/testaction/badstyle/BadStyleHelper.java
+++ src/main/java/com/example/testaction/badstyle/BadStyleHelper.java
@@ -5,22 +5,28 @@ package com.example.testaction.badstyle;
  */
 public class BadStyleHelper {
 
-	private int _counter = 0;
-	private String Bad_Name = "test";
+    private int counter = 0;
+    private String badName = "test";
 
-	public void ProcessData(int InputParam) {
-		int result_value = 0;
-		for (int i = 0; i < 5; i++) result_value += i;
-		while (result_value > 0) result_value--;
-		try {
-			Integer.parseInt("x");
-		} catch (NumberFormatException e) {
-		}
-		String veryLongLine = "Another intentionally long line exceeding 120 characters to trigger Checkstyle LineLength rule and verify the CI pipeline correctly reports style violations in multiple files";
-	}
+    public void processData(int inputParam) {
+        int resultValue = 0;
+        for (int i = 0; i < 5; i++) {
+            resultValue += i;
+        }
+        while (resultValue > 0) {
+            resultValue--;
+        }
+        try {
+            Integer.parseInt("x");
+        } catch (NumberFormatException e) {
+            throw e;
+        }
+        String veryLongLine = "Another intentionally long line exceeding 120 characters " +
+                "to trigger Checkstyle LineLength rule and verify the CI pipeline correctly " +
+                "reports style violations in multiple files";
+    }
 
-	public int GetBadValue() {
-		return _counter;
-	}
-
+    public int getBadValue() {
+        return counter;
+    }
 }
