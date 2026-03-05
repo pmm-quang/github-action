5327ed944997da0fc6fb31b39b41b1ff40b33baa
quangpmm <dung.nguyenviet1@amela.vn>
2026-03-05 16:57:09 +0900

    init

---
+++ src/main/java/com/example/testaction/TestActionApplication.java
@@ -0,0 +1,13 @@
+package com.example.testaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestActionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testaction/TestActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
